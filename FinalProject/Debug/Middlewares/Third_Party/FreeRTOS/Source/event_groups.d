2c59065e1fab064f57c63f6ef3c0a3e3ab201546
Chayakarn Thnyakorn <you@example.com>
2018-12-13 22:09:07 +0700

    For board stm32

---
+++ FinalProject/Debug/Middlewares/Third_Party/FreeRTOS/Source/event_groups.d
@@ -0,0 +1,41 @@
+Middlewares/Third_Party/FreeRTOS/Source/event_groups.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/event_groups.c \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Inc/FreeRTOSConfig.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Inc/FreeRTOSConfig.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+C:/Users/JT/Desktop/hangProject/FinalProject/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
